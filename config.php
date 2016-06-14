a1d8bbfebbe17c2b8f2bd3798ea95adaa0244c0b
Nick Foster <NickFS@users.noreply.github.com>
2016-06-13 21:08:22 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['callback_url']         =   'CALL BACK URL/?fbTrue=true'; //   /?fbTrue=true allow you to code process section.
+
+//Facebook configuration
+$config['App_ID']      =   '604916789676684';
+$config['App_Secret']  =   '5aff27768cad4c42b44c6acedb952f73'; 
+
+?>
